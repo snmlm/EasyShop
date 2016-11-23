0ea5190caaf9fa1d92cfc030cdde7109ddb2b478
wxcican <wxcican@qq.com>
2016-11-23 17:32:43 +0800

    个人信息页面，实现02

--- app/src/main/java/com/fuicuiedu/idedemo/easyshop/main/me/personInfo/PersonActivity.java
+++ app/src/main/java/com/fuicuiedu/idedemo/easyshop/main/me/personInfo/PersonActivity.java
@@ -4,16 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.fuicuiedu.idedemo.easyshop.R;
 import com.fuicuiedu.idedemo.easyshop.commons.ActivityUtils;
+import com.fuicuiedu.idedemo.easyshop.components.ProgressDialogFragment;
 import com.fuicuiedu.idedemo.easyshop.main.MainActivity;
 import com.fuicuiedu.idedemo.easyshop.model.CachePreferences;
+import com.fuicuiedu.idedemo.easyshop.model.ItemShow;
+import com.fuicuiedu.idedemo.easyshop.model.User;
 import com.hannesdorfmann.mosby.mvp.MvpActivity;
-import com.pkmmte.view.CircularImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,6 +36,10 @@ public class PersonActivity extends MvpActivity<PersonView, PersonPersenter> imp
     ListView listView;
 
     private ActivityUtils activityUtils;
+    private ProgressDialogFragment progressDialogFragment;
+    private List<ItemShow> list = new ArrayList<>();
+    private PersonAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +48,39 @@ public class PersonActivity extends MvpActivity<PersonView, PersonPersenter> imp
         ButterKnife.bind(this);
 
         activityUtils = new ActivityUtils(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        adapter = new PersonAdapter(list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(onItemClickListener);
+
+        //获取用户头像
+        // TODO: 2016/11/23 0023 未实现，无效
+        updataAvatar(CachePreferences.getUser().getHead_Image());
+    }
+
+    //方便修改完昵称，回来更改数据
+    @Override
+    protected void onStart() {
+        super.onStart();
+        list.clear();
+        init();
+        adapter.notifyDataSetChanged();
+    }
+
+    private void init(){
+        User user = CachePreferences.getUser();
+        list.add(new ItemShow(getResources().getString(R.string.username),user.getName()));
+        list.add(new ItemShow(getResources().getString(R.string.nickname),user.getNick_name()));
+        list.add(new ItemShow(getResources().getString(R.string.hx_id),user.getHx_Id()));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) finish();
+        return super.onOptionsItemSelected(item);
     }
 
     @NonNull
@@ -45,24 +89,44 @@ public class PersonActivity extends MvpActivity<PersonView, PersonPersenter> imp
         return new PersonPersenter();
     }
 
+    private AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            switch (position){
+                case 0:
+                    activityUtils.showToast(getResources().getString(R.string.username_update));
+                    break;
+                case 1:
+//                    activityUtils.startActivity("昵称页面");
+                    break;
+                case 2:
+                    activityUtils.showToast(getResources().getString(R.string.id_update));
+                    break;
+            }
+        }
+    };
+
+
     @Override
     public void showPrb() {
-
+        if (progressDialogFragment == null) progressDialogFragment = new ProgressDialogFragment();
+        if (progressDialogFragment.isVisible()) return;
+        progressDialogFragment.show(getSupportFragmentManager(),"progress_dialog_fragment");
     }
 
     @Override
     public void hidePrb() {
-
+        progressDialogFragment.dismiss();
     }
 
     @Override
     public void showMsg(String msg) {
-
+        activityUtils.showToast(msg);
     }
 
     @Override
     public void updataAvatar(String url) {
-
+        //// TODO: 2016/11/23 0023 设置头像
     }
 
     @OnClick({R.id.btn_login_out,R.id.iv_user_head})
@@ -80,7 +144,6 @@ public class PersonActivity extends MvpActivity<PersonView, PersonPersenter> imp
                 //清除所有旧的activtiy
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
-
         }
     }
 }
